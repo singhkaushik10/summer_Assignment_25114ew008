0f056844fa1c30cae38b694fb239a50be2471fbb
Kaushik singh <25114ew008@glbitm.ac.in>
2026-06-17 20:21:24 +0530

    added day_14 and day_15 program

---
+++ Day_14/Q55.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+int main()
+{
+    int arr[100],n,i;
+    int largest, second;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+
+    for(i=0; i<n;i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    largest=second=arr[0];
+
+    for(i=1;i<n;i++)
+    {
+        if(arr[i]>largest)
+        {
+            second = largest;
+            largest = arr[i];
+        }
+        else if(arr[i] > second && arr[i] != largest)
+        {
+            second = arr[i];
+        }
+    }
+
+    printf("Second Largest Element = %d", second);
+
+    return 0;
+
+
+    
+}
